0aa5628a356120257568086147e4818c8be5ca9e
kahnyee <kahnyee.22@ichat.sp.edu.sg>
2023-06-25 15:26:08 +0800

    Initial Revision for main

--- Project 5 Python Code/Main.py
+++ Project 5 Python Code/Main.py
@@ -1,15 +1,24 @@
-from hal import dht11
-from hal import hal_adc
-from hal import hal_moisture_sensor
-from hal import hal_temp_humidity_sensor
-from hal import hal_servo
-from hal import hal_led
-from hal import hal_dc_motor
-from local_hal import LDR
-from local_hal import Potentiometer
+import time
+from threading import Thread
+import queue
+
+from hal import hal_led as led
+from hal import hal_adc as adc
+from hal import hal_moisture_sensor as moisture_sensor
+from hal import hal_servo as servo
+from hal import hal_temp_humidity_sensor as temp_humid_sensor
+from hal import hal_dc_motor as dc_motor
 
 def main():
-    while True:
+    # initialization of HAL modules
+    led.init()
+    adc.init()
+    moisture_sensor.init()
+    servo.init()
+    temp_humid_sensor.init()
+    dc_motor.init()
 
-if __name__ == "__main__":
+    while (True):
+
+if __name__ == '__main__':
     main()
