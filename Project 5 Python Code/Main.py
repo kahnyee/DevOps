c0da7788f555e2195afaa821d8fd52e39fe4d2c4
kahnyee <kahnyee.22@ichat.sp.edu.sg>
2023-06-25 14:44:32 +0800

    Initial Revision for Main

---
+++ Project 5 Python Code/Main.py
@@ -0,0 +1,14 @@
+import dht11.py
+import hal_adc.py
+import hal_moisture_sensor.py
+import hal_temp_humidity_sensor.py
+import hal_servo.py
+import hal_led.py
+import hal_dc_motor.py
+import LDR.py
+import Potential_meter.py
+def main():
+    while True:
+
+if __name__ == "__main__":
+    main()
