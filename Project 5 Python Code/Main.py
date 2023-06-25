e0a32d0ac52157fe93ca9944b039abc89eb95985
kahnyee <kahnyee.22@ichat.sp.edu.sg>
2023-06-25 16:06:44 +0800

    HAL codes added

--- Project 5 Python Code/Main.py
+++ Project 5 Python Code/Main.py
@@ -17,6 +17,27 @@ from hal import hal_servo
 from hal import hal_led
 from hal import hal_dc_motor
 
+def potentiometer_reading():
+    PotReadings = adc.get_adc_value(1)
+    print(PotReadings)
+
+def temperature_reading():
+    TempReadings = temp_humid_sensor.read_temp_humidity()
+    print(TempReadings[0])
+
+def humidity_reading():
+    HumiReadings = temp_humid_sensor.read_temp_humidity()
+    print(HumiReadings[1])
+
+def ldr_reading():
+    LDRReadings = adc.get_adc_value(0)
+    print(LDRReadings)
+
+def moisture_reading():
+    MoistReadings = hal_moisture_sensor.read_sensor()
+    print(MoistReadings)
+
+
 def main():
     # initialization of HAL modules
     led.init()
@@ -26,7 +47,12 @@ def main():
     temp_humid_sensor.init()
     dc_motor.init()
 
-    while (True):
+    while True:
+        potentiometer_reading()
+        temperature_reading()
+        humidity_reading()
+        ldr_reading()
+        moisture_reading()
 
 if __name__ == '__main__':
     main()
