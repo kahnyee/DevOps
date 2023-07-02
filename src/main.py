3a0df0138ff514db4bfd38a1a05dfb69f97d5e7b
kahnyee <kahnyee.22@ichat.sp.edu.sg>
2023-07-02 14:15:46 +0800

    Updates

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import time
 from threading import Thread
 import queue
+from time import sleep as sleep
 
 from hal import hal_led as led
 from hal import hal_adc as adc
@@ -9,37 +10,30 @@ from hal import hal_servo as servo
 from hal import hal_temp_humidity_sensor as temp_humid_sensor
 from hal import hal_dc_motor as dc_motor
 
-from hal import dht11
-from hal import hal_adc
-from hal import hal_moisture_sensor
-from hal import hal_temp_humidity_sensor
-from hal import hal_servo
-from hal import hal_led
-from hal import hal_dc_motor
+
 
 def potentiometer_reading():
     PotReadings = adc.get_adc_value(1)
-    print(PotReadings)
+    print("Potentiometer: " + str(PotReadings))
 
 def temperature_reading():
     TempReadings = temp_humid_sensor.read_temp_humidity()
-    print(TempReadings[0])
+    print("Temperature: " + str(TempReadings[0]))
 
 def humidity_reading():
     HumiReadings = temp_humid_sensor.read_temp_humidity()
-    print(HumiReadings[1])
+    print("Humidity: " + str(HumiReadings[1]))
 
 def ldr_reading():
     LDRReadings = adc.get_adc_value(0)
-    print(LDRReadings)
+    print("LDR: " + str(LDRReadings))
 
 def moisture_reading():
-    MoistReadings = hal_moisture_sensor.read_sensor()
-    print(MoistReadings)
+    MoistReadings = moisture_sensor.read_sensor()
+    print("Moisture: " + str(MoistReadings))
 
 
 def main():
-    # initialization of HAL modules
     led.init()
     adc.init()
     moisture_sensor.init()
@@ -53,6 +47,8 @@ def main():
         humidity_reading()
         ldr_reading()
         moisture_reading()
+        print(" ")
+        sleep(1)
 
 if __name__ == '__main__':
     main()
