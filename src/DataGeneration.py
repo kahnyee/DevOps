d0f3445ba1a1cf7ef8a41b92c3a01233be6faa43
Pozq100 <chenjc.22@ichat.sp.edu.sg>
2023-08-06 14:04:30 +0800

    Added comments to code to improve understanability

--- src/DataGeneration.py
+++ src/DataGeneration.py
@@ -4,29 +4,39 @@ import time
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 from datetime import datetime,date
+
 csvfile = "Alldatas.csv"
 
+# Collect data from RPI and send it over to WriteData to be writen into csvfile
 def DataGeneration(Temperature, Humidity, Moisture_sensor, Potentiometer, LDR):
     Time = (time.time() + 28800000) * 1000
     WriteData([Time, Temperature, Humidity, Moisture_sensor, Potentiometer, LDR])
     return [Time, Temperature, Humidity, Moisture_sensor, Potentiometer, LDR]
 
+# Write the field names of a new csvfile
 def WriteFieldNames():
     with open(csvfile,"w") as file:
         writer = csv.writer(file)
         Data = ["Time", "Temperature", "Humidity", "Moisture_sensor", "Potentiometer", "LDR"]
         writer.writerow(Data)
     return
+
+# Write data to csvfile
 def WriteData(Datas):
+    # changing Moisture_sensor data from True/False to 1.0/0.0
     if Datas[3]:
         Datas[3] = 1.0
     else:
         Datas[3] = 0.0
+
+    # Append the data from RPI into csvfile
     with open(csvfile, 'a', newline="") as Data:
         print(Datas)
         writer = csv.writer(Data)
         writer.writerow(Datas)
     return
+
+# Read the specified line of the csvfile
 def ReadLine(csvfile,line):
     data = []
     with open(csvfile, "r") as csvfile:
@@ -37,6 +47,7 @@ def ReadLine(csvfile,line):
             data.append(float(i))      
     return data
 
+# Read the LatestLine of the csvfile
 def LatestLine(csvfile):
     totallines = 0
     with open(csvfile,"r") as file:
@@ -49,12 +60,13 @@ def LatestLine(csvfile):
         line = 3
     return line
 
+# Create a new csv file everyday with that day datas
 def save_to_csv(current_date):
     gauth = GoogleAuth()
     drive = GoogleDrive(gauth)
 
     filename = f"{current_date}.csv"
-    csv_file_path = "test_Alldatas.csv"
+    csv_file_path = "Alldatas.csv"
 
     folder_id = '1ztWIVXy_3z2zNlTQaRnAoLNic0QcYZHR'
     gfile = drive.CreateFile({'title': filename, 'parents': [{'id': folder_id}], 'mimeType': 'text/csv'})
@@ -65,10 +77,3 @@ def save_to_csv(current_date):
         gfile.SetContentString(csv_data)
         gfile.Upload()
     return
-
-def main():
-    save_to_csv()
-
-
-if __name__ == "__main__":
-    main()
